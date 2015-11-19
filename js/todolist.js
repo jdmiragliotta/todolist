865781e0d6e98e53663bb1a20648bbc7a877c5d6
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2015-11-19 14:16:24 -0500

    Fixed appending two <td> to same <tr>

--- js/todolist.js
+++ js/todolist.js
@@ -1,37 +1,40 @@
 $(document).ready(function(){
   $(".alert-danger").hide();
   
+  // Start of Adding Button 
   $(".btn-success").on("click", function(e) {
     e.preventDefault();
   
     var newTask = $("#newTask").val().trim();
     var isDuplicate = false;
     $("td.task-td").each(function(){
-      if($(this.text().trim().toLowerCase()) === newTask.toLowerCase()){
+      if($(this).text().trim().toLowerCase() === newTask.toLowerCase()) {
         isDuplicate = true;
         return;
       }
     });
 
     if(isDuplicate){
-      $(".alert-danger")
+      $(".alert-danger").slideDown();
       return;
     }
-
-    var newRow = $("<tr>");
-    var taskTd = $("<td>").addClass("task-td").append(newTask);
     
-    var deleteBtn = $("<button>").addClass("btn btn-danger").append("Delete It!");
-    var deleteTd = $("<td>").append(deleteBtn);
-    
-    var completedBtn = $("<button>").addClass("btn btn-info").append("Boom Killed It!");
-    var completedTd = $("<td>").append(completedBtn);
+    var newRow = $("<tr>");    // ADD NEW TABLE ROW
+    var taskTd = $("<td>").addClass("task-td text").append(newTask);       // ADD NEW TABLE DATA WITH CLASSES TO HOLD NEW TASK
+    var completedBtn = $("<button>").addClass("btn btn-info").append("Boom Killed It!");       // ADD NEW BUTTON WITH CLASS AND TEXT
+    var completedTd = $("<td>").append(completedBtn);      // ADD BUTTON TO NEW TABLE DATA
+    var deleteBtn = $("<button>").addClass("btn btn-danger").append("Delete It!"); // ADD NEW BUTTON WITH CLASS AND TEXT
+    var deleteTd = $("<td>").append(deleteBtn);     // ADD BUTTON INTO NEW TABLE DATA
 
-    newRow.append(taskTd).append(deleteTd);
+
+    newRow.append(taskTd).append(completedTd).append(deleteTd);
     $("tbody").append(newRow);
 
-    $("#newWord").val("").focus();
+    $("#newTask").val("").focus();
   }); // End of Adding Button
+
+  $("table")
+  
 
 
 
